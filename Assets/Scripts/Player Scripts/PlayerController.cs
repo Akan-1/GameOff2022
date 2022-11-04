cfb03b08d0ff750b5a333455d70f62a023302018
Eibone <70942967+Akan-1@users.noreply.github.com>
2022-11-04 17:55:54 +0200

    Add a simple character controller
    Add a script with player movement
    Add player physics material
    Add some default sprites

---
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        LookAtMouse();
+    }
+
+    private void FixedUpdate()
+    {
+        Walk();   
+    }
+
+    void Walk()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+    }
+
+    void LookAtMouse()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.up = (mousePos - new Vector2(transform.position.x, transform.position.y));
+    }
+
+}
