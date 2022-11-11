677f8619000b55f007ff9ef9b4562dbf3c8a1823
Voifear <114813523+Voifear@users.noreply.github.com>
2022-11-11 18:49:50 +0300

    New

--- Assets/Scripts/Player Scripts/2d/PlayerController2d.cs
+++ Assets/Scripts/Player Scripts/2d/PlayerController2d.cs
@@ -7,30 +7,53 @@ public class PlayerController2d : MonoBehaviour
     private Rigidbody2D _rb;
     private Animator _anim;
 
-    private bool _isFacingRight;
-    private bool _isGround;
-
-    [SerializeField] private Transform _groudCheck;
-
     [SerializeField] private LayerMask _whatIsGround;
 
-    [SerializeField] private float _groundCheckRadius;
+    private int _facingDirection = 1;
+
+    // переменные Bool
+    private bool _isFacingRight;
+    private bool _isGround;
+    private bool _isTouchWall;
+    private bool _isWallSliding;
+    private bool _canJump;
+
+    // переменные Transform
+    [SerializeField] private Transform _groudCheck;
+    [SerializeField] private Transform _wallCheck;
+
+    // не изменяемые переменные типа float
     private float _movementInputDirection; // хранит значение при нажатии клавиш (A, D)
+
+    // изменяемые переменные типа float
+    [SerializeField] private float _wallCheckRadius;
+    [SerializeField] private float _groundCheckRadius;
+    [SerializeField] private float _wallSlideSpeed;
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
+    [SerializeField] private float _wallHopForce;
+    [SerializeField] private float _wallJumpForce;
+
+    [SerializeField] private Vector2 _wallHopDirection;
+    [SerializeField] private Vector2 _wallJumpDirection;
 
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
+
+        _wallHopDirection.Normalize();
+        _wallJumpDirection.Normalize();
     }
     void Update()
     {
         CheckMovement();
         CheckMovementDirection();
         UpdateAnimation();
-        CheckGround();
+        CheckSurroundings();
+        WallSlide();
+        CanJump();
     }
 
     private void FixedUpdate()
@@ -51,19 +74,76 @@ public class PlayerController2d : MonoBehaviour
         }
     }
 
+    private void CanJump()
+    {
+        if(_isGround == true || _isTouchWall)
+        {
+            _canJump = true;
+        }
+        else
+        {
+            _canJump = false;
+        }
+    }
+
+    private void WallSlide() 
+    {
+        if(_isTouchWall && !_isGround && _rb.velocity.y < 0)
+        {
+            _isWallSliding = true;
+            _canJump = true;
+        }
+        else
+        {
+            _isWallSliding = false;
+            _canJump = false;
+        }
+    }
+
     private void CheckMovement()
     {
         _movementInputDirection = Input.GetAxisRaw("Horizontal"); // вносит значение при нажатии клавиш
 
-        if (Input.GetButton("Jump") && _isGround)
+        if (Input.GetButton("Jump") && _isGround && _canJump)
         {
             Jump();
+        }
+        else if(Input.GetButton("Jump") && _isWallSliding && _canJump)
+        {
+            JumpOnWall();
+        }
+    }
+
+    private void JumpOnWall()
+    {
+        if(_isWallSliding && _movementInputDirection == 0)
+        {
+            Vector2 forceToAdd = new Vector2(_wallHopDirection.x /* _wallHopForce * _facingDirection*/, _wallHopDirection.y /* _wallHopForce*/);
+            _rb.AddForce(forceToAdd, ForceMode2D.Impulse);
+            _isWallSliding = false;
+        }
+        else if((_isWallSliding || _isTouchWall) && _movementInputDirection != 0)
+        {
+            Vector2 forceToAdd = new Vector2(_wallJumpDirection.x * _wallJumpForce * _movementInputDirection, _wallJumpDirection.y * _wallJumpForce);
+            _rb.AddForce(forceToAdd, ForceMode2D.Impulse);
+            _isWallSliding = false;
         }
     }
 
     private void ApllyMovement()
     {
-        _rb.velocity = new Vector2(_movementInputDirection * _speed, _rb.velocity.y); // придаёт движение
+        if (!_isWallSliding)
+        {
+            _rb.velocity = new Vector2(_movementInputDirection * _speed, _rb.velocity.y); // придаёт движение
+        }
+
+        if (_isWallSliding)
+        {
+            if(_rb.velocity.y < -_wallSlideSpeed)
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, -_wallSlideSpeed);
+            }
+        }
     }
 
     private void Jump()
@@ -71,9 +151,11 @@ public class PlayerController2d : MonoBehaviour
         _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
     }
 
-    private void CheckGround()
+    private void CheckSurroundings()
     {
         _isGround = Physics2D.OverlapCircle (_groudCheck.position, _groundCheckRadius, _whatIsGround);
+
+        _isTouchWall = Physics2D.Raycast(_wallCheck.position, transform.right, _wallCheckRadius, _whatIsGround);
     }
 
     private void UpdateAnimation() // здесь находится вся анимация
@@ -83,13 +165,20 @@ public class PlayerController2d : MonoBehaviour
 
     private void Flip() // поворот игрока (влево, вправо)
     {
-        _isFacingRight = !_isFacingRight;
-        transform.Rotate(0, 180, 0);
+        if (!_isWallSliding)
+        {
+            _facingDirection *= -1;
+            _isFacingRight = !_isFacingRight;
+            transform.Rotate(0, 180, 0);
+        }      
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(_groudCheck.position, _groundCheckRadius);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(_wallCheck.position, _wallCheckRadius);
     }
 }
