a3fd4dba2a1b644816ef02cd5128ba5548c604e1
asneep22 <84315546+asneep22@users.noreply.github.com>
2022-11-16 02:38:41 +0500

    UpgradeZombieAI

--- Assets/Scripts/AI/Enemy.cs
+++ Assets/Scripts/AI/Enemy.cs
@@ -1,53 +1,232 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Enemy : MonoBehaviour, ITakeDamage
 {
-    [SerializeField] private float moveSpeed = 1f;
+    private enum States
+    {
+        Patroling,
+        Aggresive
+    }
+    private States _state;
+    private Transform _transform;
+    private float _animationCurveCurrentTime;
+    private Vector3 _startScale;
 
+    [Header("Characteristic")]
     [SerializeField] private int _health;
     [SerializeField] private int _damage;
 
+    [Header("Movement")]
+    [SerializeField] private AnimationCurve _speedAnimationCurve;
+    [SerializeField] [Min(0.1f)] private float _distanceToStopping;
+    [SerializeField] private float _agressiveAdditionalSpeed = 2;
+    private bool _isStopping;
+    private bool _isWait;
 
-    private Rigidbody2D rb;
+    [Header("StatePatroling")]
+    [SerializeField] private List<Transform> _patrolingPoints = new List<Transform>();
+    [SerializeField] [Min(0)] private Vector2 _stayTimeOnPointBetween;
+    private int _indexOfCurrentfPatrolingPoint;
+    private bool _isReversePatrooling;
+    private IEnumerator _patroling;
 
-    // Start is called before the first frame update
+    [Header("StateAggressive")]
+    [SerializeField] private LayerMask _characterMask;
+    [SerializeField] [Min(0)] private float _viewDistance;
+    private PlayerController2d _playerController2D;
+
+    private Rigidbody2D _rigibidy2D;
+
+    public int Health
+    {
+        get => _health;
+        set
+        {
+            GameObjectsManager.CheckLifeAmount(_health, gameObject);
+        }
+    }
+
+    public Transform Target
+    {
+        get;
+        set;
+    }
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rigibidy2D = GetComponent<Rigidbody2D>();
+        _transform = GetComponent<Transform>();
         _health = Random.Range(2, 5);
+        _startScale = _transform.localScale;
+        GoToNextPatrolingPoint();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (IsFacingRight())
+        MoveToTarget();
+        RotateToTarget();
+        View();
+    }
+
+    #region States
+
+    private void EnablePatrolingState()
+    {
+        _state = States.Patroling;
+    } 
+    private void EnableAgressiveState()
+    {
+        _state = States.Aggresive;
+    }
+
+    #endregion
+
+    #region Movement
+
+    private void MoveToTarget()
+    {
+        if (Target != null)
         {
-            rb.velocity = new Vector2(moveSpeed, 0f);
-        }
-        else
-        {
-            rb.velocity = new Vector2(-moveSpeed, 0f);
+            float distanceToTarget = Vector2.Distance(_transform.position, Target.position);
+
+            _rigibidy2D.velocity = GetMovementVector() * GetCurrentSpeed();
+
+            if (_state == States.Patroling)
+            {
+                if (distanceToTarget <= _distanceToStopping)
+                {
+                    _isStopping = true;
+
+                    if (_rigibidy2D.velocity == Vector2.zero)
+                    {
+                        WaitAndGoToNextPoint();
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("Aggressive");
+            }
         }
     }
 
-    private bool IsFacingRight()
+    private Vector2 GetMovementVector()
     {
-        return transform.localScale.x > Mathf.Epsilon;
+        Vector2 toTargetVector = new Vector2(Target.position.x - _transform.position.x, _transform.position.y).normalized;
+        return toTargetVector;
     }
+
+    private float GetCurrentSpeed()
+    {
+        float additionSpeed = _state == States.Aggresive ? _agressiveAdditionalSpeed : 0;
+        _animationCurveCurrentTime = _isStopping ? _animationCurveCurrentTime - Time.deltaTime : _animationCurveCurrentTime + Time.deltaTime;
+        _animationCurveCurrentTime = Mathf.Clamp(_animationCurveCurrentTime, 0, _speedAnimationCurve.keys[_speedAnimationCurve.length - 1].time);
+        return _speedAnimationCurve.Evaluate(_animationCurveCurrentTime) + additionSpeed;
+    }
+
+    private void RotateToTarget()
+    {
+        if (Target != null)
+        {
+            _transform.localScale = Target.position.x > _transform.position.x ? _startScale : new Vector3(-_startScale.x, _startScale.y, _startScale.z);
+        }
+    }
+
+    private void WaitAndGoToNextPoint()
+    {
+        if (!_isWait)
+        {
+            Target = null;
+            _isWait = true;
+            Invoke(nameof(GoToNextPatrolingPoint), Random.Range(_stayTimeOnPointBetween.x, _stayTimeOnPointBetween.y));
+        }
+    }
+
+    #endregion
+
+    #region Patroling
+
+    private void GoToNextPatrolingPoint()
+    {
+        _indexOfCurrentfPatrolingPoint = GetNextIndexOfPatroolingPoint();
+        _isStopping = false;
+        _isWait = false;
+
+        Target = _patrolingPoints[_indexOfCurrentfPatrolingPoint];
+    }
+
+    private int GetNextIndexOfPatroolingPoint()
+    {
+        bool isEndIndex = _indexOfCurrentfPatrolingPoint == _patrolingPoints.Count - 1;
+        bool isStartIndex = _indexOfCurrentfPatrolingPoint == 0;
+
+        if (isEndIndex || isStartIndex)
+        {
+            _isReversePatrooling = !_isReversePatrooling;
+        }
+
+        return _isReversePatrooling ? _indexOfCurrentfPatrolingPoint + 1: _indexOfCurrentfPatrolingPoint - 1;
+    }
+    #endregion
+
+    #region Aggresive
+
+    public void View()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(_transform.position, _transform.right * transform.localScale.x, _viewDistance, _characterMask);
+
+        if (raycastHit2D.collider != null)
+        {
+            if (raycastHit2D.collider.TryGetComponent(out PlayerController2d playerController2D))
+            {
+                _playerController2D = playerController2D;
+                Target = _playerController2D.transform;
+                EnableAgressiveState();
+            }
+        }
+
+        if (_playerController2D != null)
+        {
+            float _distanceToPlayer = Vector2.Distance(_transform.position, _playerController2D.transform.position);
+            Debug.Log(_distanceToPlayer);
+
+            if (_distanceToPlayer > _viewDistance)
+            {
+                _playerController2D = null;
+                WaitAndGoToNextPoint();
+                EnablePatrolingState();
+            }
+        }
+
+    }
+
+    #endregion
+
+    #region Damage
 
     public void TakeDamage(int damage)
     {
         _health -= damage;
-        GameObjectsManager.CheckLifeAmount(_health, gameObject);
+
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
-
-        if(collision.gameObject.TryGetComponent(out ITakeDamage takeDamage))
+    /*    private void OnTriggerEnter2D(Collider2D collision)
         {
-            takeDamage.TakeDamage(_damage);
-        }
+            transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
+
+            if(collision.gameObject.TryGetComponent(out ITakeDamage takeDamage))
+            {
+                takeDamage.TakeDamage(_damage);
+            }
+        }*/
+
+    #endregion
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.right * transform.localScale.x * _viewDistance);
     }
 }
